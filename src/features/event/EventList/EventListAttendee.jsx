be9433b37e3995bd51efd825f0c8d36ccf4a8b55
naoto.fj <naoto.fj@gmail.com>
2020-04-09 02:18:00 -0300

    End of section 5

--- src/features/event/EventList/EventListAttendee.jsx
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Image, List } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Image, List } from "semantic-ui-react";
 
 class EventListAttendee extends Component {
-	render() {
-      const {attendee} = this.props
-		return (
-         <List.Item>
-            <Image as='a' size="mini" circular src={attendee.photoURL}/>
-         </List.Item>
-      )
-	}
+  render() {
+    const { attendee } = this.props;
+    return (
+      <List.Item>
+        <Image as='a' size='mini' circular src={attendee.photoURL} />
+      </List.Item>
+    );
+  }
 }
 
 export default EventListAttendee;
